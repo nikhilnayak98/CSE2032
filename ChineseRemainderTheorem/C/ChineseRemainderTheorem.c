751bfd2558dafd2a8a600628ffbd9813f0e5610d
Nikhil <nikhilnayak98@gmail.com>
2018-01-26 17:43:10 +0530

    different algorithm

--- ChineseRemainderTheorem/C/ChineseRemainderTheorem.c
+++ ChineseRemainderTheorem/C/ChineseRemainderTheorem.c
@@ -5,80 +5,34 @@ Desc - Chinese Remainder Theorem
 */
 #include "stdio.h"
 int gcd(int, int);
-int check(int[], int);
-int evaluate(int[], int[], int);
 void main() 
 {
-    int n = 4, i, c[n], m[n];
+    int y, b, c, m, n;
 
-    printf("\nx ≅ c(mod m)\n");
-    for(i = 0; i < n; i++) 
+    printf("\nx ≅ b(mod m)");
+    
+    printf("\nEnter b - ");
+    scanf("%d", &b);
+    
+    printf("\nEnter m - ");
+    scanf("%d", &m);
+    
+    printf("\nx ≅ c(mod n)");
+    
+    printf("\nEnter c - ");
+    scanf("%d", &c);
+    
+    printf("\nEnter n - ");
+    scanf("%d", &n);
+    
+    if(gcd(m, n) == 1)
     {
-        printf("\nEnter c - ");
-        scanf("%d", & c[i]);
-
-        printf("\nEnter m - ");
-        scanf("%d", & m[i]);
-
-        printf("\n");
+    	for(y = 0; y < n; y++)
+    	{
+    		if((m * y) % n == (c - b))
+    			printf("\nx ≅ %d(mod %d)\n", y, n);
+    	}
     }
-
-    if(!check(m, n))
-        printf("\nx ≅ %d \n", evaluate(c, m, n));
-    else
-        printf("\nNo solutions exist.\n");
-}
-
-// Chinese Remainder Theorem
-int evaluate(int c[], int m[], int n) 
-{
-    int Minv[n], q, r, r1, r2, t, t1, t2, k, total = 1, x = 0;
-
-    for(k = 0; k < n; k++)
-        total *= m[k];
-
-    for(k = 0; k < n; k++) 
-    {
-        r1 = m[k];
-        r2 = total / m[k];
-        t1 = 0;
-        t2 = 1;
-
-        while(r2 > 0) 
-        {
-            q = r1 / r2;
-            r = r1 - q * r2;
-            r1 = r2;
-            r2 = r;
-
-            t = t1 - q * t2;
-            t1 = t2;
-            t2 = t;
-        }
-
-        if(r1 == 1)
-            Minv[k] = t1;
-
-        if(Minv[k] < 0)
-            Minv[k] = Minv[k] + m[k];
-    }
-
-    for(k = 0; k < n; k++)
-        x += (c[k] * total * Minv[k]) / m[k];
-
-    return x;
-}
-
-int check(int m[], int n) 
-{
-    int x, y;
-    for(x = 0; x < n; x++) 
-    {
-        for(y = x + 1; y < n; y++)
-            if(gcd(m[x], m[y]) != 1)
-                return 1;
-    }
-    return 0;
 }
 
 int gcd(int a, int b) 
