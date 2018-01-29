5ea1caf017b654604351b0206e077f2c447beb9b
Nikhil <nikhilnayak98@gmail.com>
2018-01-29 22:11:58 +0530

    ported CRT to java

---
+++ ChineseRemainderTheorem/Java/ChineseRemainderTheorem.java
@@ -0,0 +1,48 @@
+/*
+Name - Nikhil Ranjan Nayak
+Regd no - 1641012040
+Desc - Chinese Remainder Theorem
+*/
+import java.util.Scanner;
+
+public class ChineseRemainderTheorem {
+
+	public static void main(String[] args)  {
+    Scanner sc = new Scanner(System.in);
+    int y, b, c, m, n, x;
+    System.out.println("\nx ≅ b(mod m)");
+    
+    System.out.println("\nEnter b - ");
+    b = sc.nextInt();
+    
+    System.out.println("\nEnter m - ");
+    m = sc.nextInt();
+    
+    System.out.println("\nx ≅ c(mod n)");
+    
+    System.out.println("\nEnter c - ");
+    c = sc.nextInt();
+    
+    System.out.println("\nEnter n - ");
+    n = sc.nextInt();
+    
+    if(gcd(m, n) == 1)
+    {
+    	for(y = 0; y < n; y++)
+    	{
+    		if((m * y) % n == (c - b))
+            {
+                x = (m * y) + b;
+    			System.out.println("\nx = " + x);
+                break;
+            }
+    	}
+    }
+}
+
+	public static int gcd(int a, int b) {
+    	if(b == 0)
+        	return a;
+    	return gcd(b, a % b);
+    }
+}
---
+++ EulersPhiFunction/C/prog.c
@@ -0,0 +1,12 @@
+/*
+Name - Nikhil Ranjan Nayak
+Regd no - 1641012040
+Desc - Euler's Phi Function
+*/
+#include "stdio.h"
+void main()
+{
+	int n;
+	printf("\nEnter n - ");
+	scanf("%d", &n);
+}
