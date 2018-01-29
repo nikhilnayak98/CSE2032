23a6b1688fd26549e0461b891b549c8c5eb0a360
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-01-29 22:35:44 +0530

    Update EulersPhiFunction.c

--- EulersPhiFunction/C/EulersPhiFunction.c
+++ EulersPhiFunction/C/EulersPhiFunction.c
@@ -14,16 +14,16 @@ void main()
 	temp = n;
 	
 	for(i = 2; i <= sqrt(n); i++)
-    {
-        if(n % i == 0)
-        { 
-            while(n % i == 0)
+    	{
+		if(n % i == 0)
+		{
+			while(n % i == 0)
 			{
-                n /= i;
+				n /= i;
 			}
-            res -= (res / i);
-        }
-    }
+			res -= (res / i);
+		}
+	}
 
     if(n > 1)
         res -= (res / n);
