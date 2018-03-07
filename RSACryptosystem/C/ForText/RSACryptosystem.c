c7b0eaaf626e37a1be981886d5e028a615c51f40
Nikhil Nayak <nikhilnayak98@gmail.com>
2018-03-07 18:31:03 +0530

    better prime checker algorithm

--- RSACryptosystem/C/ForText/RSACryptosystem.c
+++ RSACryptosystem/C/ForText/RSACryptosystem.c
@@ -97,7 +97,7 @@ long int gcd(long int a, long int b)
 long int isprime(long int a)
 {
     int i;
-    for(i = 2; i < a; i++)
+    for(i = 2; i <=sqrt(a); i++)
     {
         if((a % i) == 0)
             return 0;
